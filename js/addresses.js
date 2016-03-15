7655a4481b262f31f8288d533d9746865224fae9
Ben <BforBen@users.noreply.github.com>
2016-03-15 14:38:36 +0000

    Add error message when token usage exceeded

--- js/addresses.js
+++ js/addresses.js
@@ -31,8 +31,19 @@
         responsePromise.then(function(response) {
           $scope.addresses.data = response.data;
           $scope.addresses.isShowMessage = (response.data.length == 0);
+          $scope.addresses.message = (response.data.length == 0) ? 'No addresses found for ' + $scope.addresses.query + '.' : '';
         }, function(response) {
-          return $scope.addresses.message = "Error in searching for address data.";
+          var statusCode = response.status;
+          $scope.addresses.isShowMessage = true;
+          $scope.addresses.messageClass = 'danger';
+          $scope.addresses.data = [];
+          switch (statusCode) {
+            case 429:
+              $scope.addresses.message = 'Sorry, the maximum number of searches per day has been exceeded.';
+              break;
+            default:
+              $scope.addresses.message = 'Sorry, there was an error whilst searching. [' + response.statusText + ']';
+          }
         });
       };
     }
